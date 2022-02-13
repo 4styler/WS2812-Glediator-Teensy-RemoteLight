aff8c08e18748df84aa04098e71b267ee919d0bc
4styler <4styler@gmx.de>
2022-02-13 19:43:02 +0100

    Update WS2812_Glediator_Teensy_LC.ino

--- WS2812_Glediator_Teensy_LC.ino
+++ WS2812_Glediator_Teensy_LC.ino
@@ -1,18 +1,13 @@
-//Merged from:
-//https://github.com/PaulStoffregen/WS2812Serial and
-//from pjrc forum
-
 #include <WS2812Serial.h>
 #define USE_WS2812SERIAL
 #include <FastLED.h>
 
 // How many leds in your strip?
-#define NUM_LEDS 10
+#define NUM_LEDS 60
 
 // Which pin are you use?
 // Usable pins:
-//   Teensy LC:   1, 4, 5, 24 (You can connect one of these Pins to Pin 17 to get 5v on Pin "17 vin Voltage"
-                  //https://www.pjrc.com/teensy/teensyLC.html or https://github.com/PaulStoffregen/WS2812Serial (See Picture))
+//   Teensy LC:   1, 4, 5, 24
 //   Teensy 3.2:  1, 5, 8, 10, 31   (overclock to 120 MHz for pin 8)
 //   Teensy 3.5:  1, 5, 8, 10, 26, 32, 33, 48
 //   Teensy 3.6:  1, 5, 8, 10, 26, 32, 33
@@ -35,7 +30,7 @@ void setup() {
   delay(1500);  // startup delay
   LEDS.addLeds<WS2812SERIAL,DATA_PIN,BGR>(leds,NUM_LEDS);
   LEDS.setBrightness(BRIGHTNESS);
-  
+  startupLEDsTest();
 }
 
 int serialGlediator() {
@@ -43,14 +38,50 @@ int serialGlediator() {
   return Serial.read();
 }
 
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
 byte r,g,b;
 int i;
 
 void loop() {
+  
+   while (!Serial.available()) {
+     static uint8_t hue = 0;
+     Serial.print("x");
+      // First slide the led in one direction
+      for(int i = 0; i < NUM_LEDS; i++) {
+        // Set the i'th led to red
+        leds[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show();
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(20);
+      }
+      Serial.print("x");
+    
+      // Now go in the other direction.
+      for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+        // Set the i'th led to red
+        leds[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show();
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(20);
+      }
+   }
+   
    while (serialGlediator () != CMD_NEW_DATA) {}
-       Serial.readBytes((char*)leds, NUM_LEDS * 3);
-       FastLED.show();
+   Serial.readBytes((char*)leds, NUM_LEDS * 3);
+   FastLED.show();
+
    }                                   
+
 
 void startupLEDsTest() {
   // startup blink test to confirm LEDs are working.
